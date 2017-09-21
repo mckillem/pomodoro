68dd7ee6b76f96e7dcd4aa1e2bbe8d4eef4191ba
Matěj <mckillem@icloud.com>
2017-09-21 13:38:22 +0200

    Splňuje zadání, stačí nastylovat

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
-$("#startButton").click(function(e) {
-    alert("baf");
-});
+var interval;
+
+function setTime() {
+    var duration = document.getElementById("duration");
+    console.log(duration.value);
+    startTimer(duration.value);
+}
+
+function startTimer(duration) {
+    var duration = 60 * duration;
+    
+    var display = document.getElementById('div');
+    var timer = duration, minutes, seconds;
+    interval = setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            clearInterval(interval);
+        }
+    }, 1000);
+}
+
+function stop() {
+    clearInterval(interval);        
+}
+
+function reset() {
+    var display = document.getElementById('div');
+    var duration = document.getElementById("duration");
+    display.textContent = duration.value + ":00";
+    clearInterval(interval);
+}
+
+window.onload = function () {        
+};
